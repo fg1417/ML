b4f5d0107e495299009e655651c7e811397fef22
Fei <fei_gao0128@163.com>
2018-05-18 23:16:02 +0100

    problematic

--- Coursera_Ng/machine-learning-ex1/ex_new_1/gradientDescent.py
+++ Coursera_Ng/machine-learning-ex1/ex_new_1/gradientDescent.py
@@ -1,2 +1,16 @@
 # Compute the cost function
 # Update weights
+
+import numpy as np
+
+def computeCost(learning_rate, w, x, y = None):
+    bias = np.ones(len(y))
+    x = np.column_stack((x, bias))
+    p = np.sum(x * w, axis = 1)
+    if y == None:
+        return p
+    else:
+        delta = np.dot((p - y).transpose(), x)/len(p)
+        w = w - learning_rate * delta
+        # cost = 0.5*np.sum(np.dot(err.transpose(), err))/len(err)
+        return w
--- Coursera_Ng/machine-learning-ex1/ex_new_1/multiRegression.py
+++ Coursera_Ng/machine-learning-ex1/ex_new_1/multiRegression.py
@@ -2,6 +2,21 @@
 
 import loadData
 import featureNormalize
+import normalEqn
+import gradientDescent
+import numpy
 
-x, y = loadData.loadData('ex1data2.txt', 2)
-featureNormalize.featureNormalize(x, 2)
+def init(number):
+    return numpy.random.normal(0, 1, number + 1)
+
+def multiRegression(number, x, y, learning_rate = 1e-3, epoch = 50):
+    weights = init(number)
+    for i in range(epoch):
+        weights = gradientDescent.computeCost(learning_rate, weights, x, y)
+    return weights
+
+if __name__ == '__main__':
+    x, y = loadData.loadData('ex1data2.txt', 2)
+    x = featureNormalize.featureNormalize(x, 2)
+    print normalEqn.normalEqn(x, y)
+    print multiRegression(2, x, y)
