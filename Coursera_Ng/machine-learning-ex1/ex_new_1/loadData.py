d4be28c49518e0f85fa481f6169b8395a75eb411
Fei <fei_gao0128@163.com>
2018-05-18 11:46:38 +0100

    loadData

--- Coursera_Ng/machine-learning-ex1/ex_new_1/loadData.py
+++ Coursera_Ng/machine-learning-ex1/ex_new_1/loadData.py
@@ -1,2 +1,19 @@
 # Load data from txt files
 # Transfer into matrices
+
+import numpy
+
+def loadData(datafile, number):
+    data = numpy.loadtxt(datafile, delimiter = ',')
+    x = []
+    y = []
+    for ele in data:
+        x.append(ele[:number])
+        y.append(ele[number])
+    return x, y
+
+
+if __name__ == '__main__':
+    x, y = loadData('ex1data2.txt', 2)
+    print len(x[0])
+    print y
